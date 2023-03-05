e4983a1fd09e68acb94def391f0aab104e6e2661
udiscopotato <subhamjena68@gmail.com>
2023-03-05 23:16:58 +0530

    project started
    first commit

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Deposit some money
+// 2. Determin enumber of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winning
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+function deposit(){
+   const depositAmount = prompt("Enter a deposit amount: ") 
+}
+
+deposit()
